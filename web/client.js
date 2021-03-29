38ce7663ee5db76a8219cfea8a4da0c630b89fba
pietvanzoen <hi@pietvanzoen.com>
2021-03-29 20:17:21 +0200

    fix dataset environment variable

--- web/client.js
+++ web/client.js
@@ -1,7 +1,7 @@
 const sanityClient = require('@sanity/client')
 const client = sanityClient({
   projectId: 'fyaw7rw2',
-  dataset: process.env.SANITY_DATASET || 'development',
+  dataset: process.env.SANITY_STUDIO_API_DATASET || 'development',
   token: '', // or leave blank to be anonymous user
   useCdn: false // `false` if you want to ensure fresh data
 })
